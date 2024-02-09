263f2f73265c63bf2ed0e791a9f5ca9d60346665
anarg1812 <147712294+anarg1812@users.noreply.github.com>
2024-02-09 11:32:06 +0000

    generador de nombres de dominio

---
+++ app.js
@@ -0,0 +1,12 @@
+let pronoum = ['the', 'our'];
+let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  for (let i = 0; i < pronoum.length; i++) {
+    for (let k = 0; k < adj.length; k++) {
+        for (let e = 0; e < noun.length; e++) {
+            console.log(pronoum[i] + adj[k] + noun[e] +".com");
+
+        }
+    }
+  }
